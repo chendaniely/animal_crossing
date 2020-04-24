d5688d1578edf638e9f3b45802639b42b7b6f229
Daniel Chen <chendaniely@gmail.com>
2020-04-24 01:06:57 -0400

    update rdata and rpkg

---
+++ scripts/acnh/99-write_rdata.R
@@ -0,0 +1,23 @@
+library(readr)
+library(purrr)
+library(fs)
+library(here)
+library(glue)
+
+clean_dat <- fs::dir_ls(here::here("data", "final", "without_raw_html"))
+
+dat_f <- fs::path_file(clean_dat) %>%
+    fs::path_ext_remove()
+
+dfs <- purrr::map(
+    clean_dat,
+    readr::read_tsv
+)
+
+
+rds_pth <- here::here("pkg", "R", "animalcrossing", "data", glue::glue("{dat_f}.RData"))
+
+purrr::walk2(dfs, rds_pth,
+             function(x, y) {
+                 save(x, file = y)
+             })
